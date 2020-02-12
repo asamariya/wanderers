b6cba3800ce6b9c024435a4210ab42d5fe6fb9ad
Abhishek Samariya <asamariya@gmail.com>
2020-02-13 07:00:14 +1100

    Added multiple transition trigger points and the previous transition

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,9 @@ barba.init({
   transitions: [
     {
       name: 'next',
+      custom: ({ current, next, trigger }) => {
+        return trigger.classList && trigger.classList.contains('next');
+      },
       leave: ({ current, next, trigger }) => {
         return new Promise(resolve => {
           const timeline = gsap.timeline({
@@ -81,6 +84,72 @@ barba.init({
             .to(navigation, { opacity: 1 }, 0)
             .to(photos, { opacity: 1, x: 0 }, 0)
             .to(wiper, { x: '100%' }, 0);
+        });
+      }
+    },
+    {
+      name: 'previous',
+      leave: ({ current, next, trigger }) => {
+        return new Promise(resolve => {
+          const timeline = gsap.timeline({
+            onComplete: () => {
+              current.container.remove();
+              resolve();
+            }
+          });
+
+          const navigation = current.container.querySelectorAll(
+            'header, a.next, a.previous'
+          );
+          const photos = current.container.querySelectorAll('.photos');
+
+          timeline
+            .set(wiper, { x: '100%' })
+            .set(wiperImage, { opacity: 1 })
+            .set(wiperText, { y: '100%' })
+            .to(navigation, { opacity: 0 }, 0)
+            .to(photos, { opacity: 0.25, x: -500 }, 0)
+            .to(wiper, { x: 0 }, 0);
+        });
+      },
+      beforeEnter: ({ current, next, trigger }) => {
+        wiperText.innerHTML = next.container.getAttribute('data-title');
+
+        return new Promise(resolve => {
+          const timeline = gsap.timeline({
+            defaults: {
+              duration: 1
+            },
+            onComplete: () => {
+              resolve();
+            }
+          });
+          timeline
+            .to(wiperImage, { opacity: 1 }, 0)
+            .to(wiperText, { y: 0 }, 0)
+            .to(wiperText, { y: '100%' }, 2)
+            .to(wiperImage, { opacity: 0 }, 2);
+        });
+      },
+      enter: ({ current, next, trigger }) => {
+        return new Promise(resolve => {
+          const timeline = gsap.timeline({
+            onComplete: () => {
+              resolve();
+            }
+          });
+
+          const navigation = next.container.querySelectorAll(
+            'header, a.next, a.previous'
+          );
+          const photos = next.container.querySelectorAll('.photos');
+
+          timeline
+            .set(navigation, { opacity: 0 })
+            .set(photos, { opacity: 0.25, x: 500 })
+            .to(navigation, { opacity: 1 }, 0)
+            .to(photos, { opacity: 1, x: 0 }, 0)
+            .to(wiper, { x: '-100%' }, 0);
         });
       }
     }
